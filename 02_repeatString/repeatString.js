b5b7fe5854aa7fcedf1af51870781f40661185ba
mttt <groobytt@gmail.com>
2022-08-21 22:51:10 +0200

    complete tasks 1-3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str,times) {
+    let i
+    let result=''
+    if(times>=0){
+        for(i=0;i<times;i++){
+            result+=str
+        }
+    }else{
+        return 'ERROR'
+    }
+    return result
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let list=[]
+    let j = str.length-1
+    for (let i = 0; i < str.length; i++) {
+        list[j] = str[i]
+        j--
+    }
+        
+    let result=list.join('')
+    console.log(result)
+    return result
 
 };
 
