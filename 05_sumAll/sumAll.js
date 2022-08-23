a658c49781929c7f1794a34643a722c972a39d18
mttt <groobytt@gmail.com>
2022-08-23 17:21:39 +0200

    finish taks 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,21 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
+
+    if(a>=0 && b>=0 && Number.isInteger(a) && Number.isInteger(b)){
+        if(a>b){
+            let c=a
+            a=b
+            b=c
+        } 
+        let sum = 0
+        for(let i=a;i<=b;i++){
+            sum+=i
+
+        }
+        return sum
+
+    }else{
+        return 'ERROR'
+    }
 
 };
 
