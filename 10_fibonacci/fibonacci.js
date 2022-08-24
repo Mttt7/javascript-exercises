2d1801fbde6f93905446a5fcb4f4cdecdd20073d
mttt <groobytt@gmail.com>
2022-08-24 12:05:59 +0200

    finish task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(ind) {
+    if(typeof ind =='string'){
+        ind=Number(ind)
+    }
+    if(ind>=0){
+        let arrFib=[0,1]
+        for(let i=2;i<=ind;i++){
+            arrFib.push(arrFib[i-2]+arrFib[i-1])
+        }
+    return arrFib[ind]
+    }else return 'OOPS'
 };
 
 // Do not edit below this line
