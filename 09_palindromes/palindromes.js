024263d034c0ef217a7c16f22678b289b1a0bdb7
mttt <groobytt@gmail.com>
2022-08-24 11:51:37 +0200

    finish task 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let validStr=''
+    for(let i=0;i<str.length;i++){
+        if(str[i].match(/[a-z]/i)){
+            validStr+=str[i]
+        }
+    }
+    validStr=validStr.toLowerCase()
 
+    let ReversedStr=''
+    for(let i =validStr.length-1;i>=0;i--){
+        ReversedStr+=validStr[i]
+    }
+    if(ReversedStr===validStr) return true
+    else return false
 };
 
 // Do not edit below this line
