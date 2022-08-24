d8eebd0e9a38f9a8f1f74345da52993fade7c157
mttt <groobytt@gmail.com>
2022-08-24 10:30:13 +0200

    finish task 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,20 @@
-const ftoc = function() {
-
+const ftoc = function(ftemp) {
+  if(ftemp==32) return 0
+  else{
+  let ctemp=+(((ftemp-32)*5)/9).toFixed(1)
+  return ctemp 
+  }
 };
 
-const ctof = function() {
-
+const ctof = function(ctemp) {
+  if(ctemp==32) return 0
+  else{
+    let ftemp=+(1.8*ctemp+32).toFixed(1)
+    return ftemp
+  }
 };
+
+
 
 // Do not edit below this line
 module.exports = {
