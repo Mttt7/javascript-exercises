9f18dc9bab9c7f01c888336417636187329540ce
mttt <groobytt@gmail.com>
2022-08-24 11:09:58 +0200

    finish task 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,42 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  console.log(nums)
+  console.log('len of nums', nums.length)
+  if (nums.length == 0) return 0
+  else {
+    let sum = 0
+    for (let i=0;i<nums.length;i++) {
+      sum += Number(nums[i])
+    }
+    return sum
+  }
 };
 
-const multiply = function() {
-
+const multiply = function(nums) {
+  let result = 1
+  for(let i=0;i<nums.length;i++){
+    result*=nums[i]
+  }
+  return result
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let result=1
+	for(let i=1;i<=num;i++){
+    result*=i
+  }
+  return result
 };
 
 // Do not edit below this line
