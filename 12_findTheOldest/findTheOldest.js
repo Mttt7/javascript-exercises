164cb338906039358ed28ca55816841635ca7996
mttt <groobytt@gmail.com>
2022-08-26 08:52:26 +0200

    finish task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+  
+    let currYear = new Date()
+    let oldestPerson
+    let oldestAge=0
 
+  
+    for(let i=0;i<people.length;i++){
+      
+      if(people[i].yearOfDeath==undefined){
+         let age = currYear.getFullYear()-people[i].yearOfBirth
+
+         if(age>oldestAge){
+            oldestAge=age
+            oldestPerson=people[i]
+          }
+        }
+
+        if(Number.isInteger(people[i].yearOfDeath)){
+          let age = people[i].yearOfDeath-people[i].yearOfBirth
+
+          if(age>oldestAge){
+            oldestAge=age
+            oldestPerson=people[i]
+          }
+        }
+        
+    }
+    return oldestPerson
 };
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
